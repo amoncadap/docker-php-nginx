7b4eb84416ca3082487741cf5cbf08c940b17048
Alejandro Moncada <amoncadap@gmail.com>
2023-02-20 16:15:48 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class Data(BaseModel):
+    msg: str
+
+@app.post("/")
+def handle_post(data: Data):
+    return data
